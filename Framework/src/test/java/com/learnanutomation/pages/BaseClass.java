57211b4d79c8d1b2faf797660799a3da6abdf6d0
mpatel <mpatel@WC-IT1065W10.intranet.naylor>
2019-02-20 14:43:02 -0600

    parameters

--- Framework/src/test/java/com/learnanutomation/pages/BaseClass.java
+++ Framework/src/test/java/com/learnanutomation/pages/BaseClass.java
@@ -10,11 +10,11 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
-
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+import com.beust.jcommander.Parameters;
 import com.learnautomation.utility.BrowserFactory;
 import com.learnautomation.utility.ConfigDataProvider;
 import com.learnautomation.utility.ExcelDataProvider;
@@ -44,12 +44,15 @@ public class BaseClass {
 		Reporter.log("setting Done - Test can be started", true);
 		
 	}
+	@org.testng.annotations.Parameters({"browser","urlToBeTested"})
 	@BeforeClass
-	public void setup()
+	public void setup(String browser, String url)
 	{		
 		
 		Reporter.log("Trying to start Browser and Getting application ready", true);
-		driver=BrowserFactory.startApplication(driver, config.getBrowser(), config.getStagingURL());
+		//driver=BrowserFactory.startApplication(driver, config.getBrowser(), config.getStagingURL());
+
+		driver=BrowserFactory.startApplication(driver,browser,url);
 		Reporter.log("Browser and Application running", true);
 		
 	}
